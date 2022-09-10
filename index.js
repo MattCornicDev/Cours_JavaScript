d833a703587baa602c593e0631b52deb74174891
MattCornicDev <glgharib@gmail.com>
2022-09-11 00:15:46 +0200

    quelques fonctions pour les tableaux

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let nom = `Rarib`;
-let presentation = `Salut à tous mon prénom est ${nom}`;
+let developers = ["Rarib LAAMIMAT", "Simone WEIL", "Jeremy LEMOINE", 3];
 
-console.log(presentation);
+console.log(developers[0]);
+let nbreDePersonnes = developers.length;
+console.log(nbreDePersonnes)
+developers.push("Fred LELOUCH");
+console.log(developers)
+developers.unshift("Severine PALAMBA")
+console.log(developers)
+developers.pop()
+console.log(developers)
+
+
+
+
