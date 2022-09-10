69732abb5abe9b6b25b82fc9bff1a762c747af5d
MattCornicDev <glgharib@gmail.com>
2022-09-11 01:38:23 +0200

    les conditions

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+let estConnecte = false;
+
+if (estConnecte) {
+ console.log("Connexion établie")
+} else {
+ console.log("Connexion échoué")
+}
+
+
+let scorePSG = 12;
+let scoreRealMadrid = 12;
+
+if (scorePSG > scoreRealMadrid){
+ console.log("Vous êtes en train de rêver il est temps d'aller au boulot")
+}else if (scorePSG < scoreRealMadrid) {
+ console.log("Bah logique !")
+}else {
+ console.log("Quoi ! match null !!")
+}
+*/
+/*
+
+let maVariable = true;
+
+
+if (maVariable){
+ let monMessage = "Bienvenu dans votre page de profil"
+}else {
+ let monMessage = "Vous n'etes pas connecté"
+}
+console.log(monMessage)
+*/
