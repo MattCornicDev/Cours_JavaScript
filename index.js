63850600c686f6b3c565a8e29c566ddd13ea75a1
MattCornicDev <glgharib@gmail.com>
2022-09-10 02:07:54 +0200

    Creation d'un objet Livre

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-let nom = `Rarib`;
-let presentation = `Salut à tous mon prénom est ${nom}`;
+/*
+let livre = {
+    titre: "Harry Potter et le Prince de sang-mêlé",
+    auteur: "J.K. Rowling",
+    pages: 720,
+    disponible: true
+}
+let titreDuLivre = livre.titre;
+let nbreDePages = livre.pages;
 
-console.log(presentation);
+console.log(livre.disponible)
+*/
+
+
+class Livre {
+    constructor(titre, auteur, pages) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.pages = pages;
+    }
+}
+
+
+/*
+let monLivre = new Livre("La planète des singes", "Pierre Boulle", 274);
+console.log(monLivre.titre)
+*/
+
+/*//Cette ligne crée l'objet suivant :
+{
+    title: "La planète des singes",
+    author: "Pierre Boullon",
+    pages: 274
+}*/
+
