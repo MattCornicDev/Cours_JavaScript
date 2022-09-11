195ae9291254d92a3998c21979ef430ec2a2e9dd
MattCornicDev <glgharib@gmail.com>
2022-09-11 23:23:31 +0200

    Un exemple de boucle

---
+++ index.js
@@ -0,0 +1,11 @@
+/*let i;
+for(i = 0; i < 10; i++ ) {
+    console.log(i);
+}*/
+/*
+let text = ""
+for (let i = 0; i < 5; i++) {
+    text += "Le nombre est: " + i + "<br>";
+}
+console.log(text)
+*/
