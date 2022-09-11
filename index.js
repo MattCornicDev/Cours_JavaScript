876ae06aeaa2a15708bde72c9c433fe84ba542ed
MattCornicDev <glgharib@gmail.com>
2022-09-11 23:55:33 +0200

    exemple de fonction

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function maFonction(parametre1, parametre2) {
+    console.log('Première valeur: ' + parametre1);
+    console.log('Deuxième valeur: ' + parametre2);
+}
+
+// On exécute la fonction
+maFonction(33, "ici s'affiche le second parametre");
+
